5a992075038b6ccacf781d5fe7ac406f0958d223
Rory Duncan <robust.rory@gmail.com>
2025-02-16 22:29:54 -0800

    whatever

--- DesktopFriend.Program/DesktopFriend.cs
+++ DesktopFriend.Program/DesktopFriend.cs
@@ -18,7 +18,7 @@ public class DesktopFriend : Game
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
-        IsMouseVisible = true;
+        IsMouseVisible = false;
     }
 
     protected override void Initialize()
