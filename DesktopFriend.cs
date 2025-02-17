fd0db65aad1442aa29590da7b22be1f04876ed4e
Rory Duncan <robust.rory@gmail.com>
2025-02-16 21:55:37 -0800

    Setup build and simple ball-mouse thing

--- DesktopFriend.cs
+++ DesktopFriend.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Content;
 
 namespace desktop_friend;
 
@@ -8,6 +11,8 @@ public class DesktopFriend : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    private Texture2D ballTexture;
+    Vector2 ballPosition;
 
     public DesktopFriend()
     {
@@ -18,8 +23,9 @@ public class DesktopFriend : Game
 
     protected override void Initialize()
     {
+        _spriteBatch = new SpriteBatch(_graphics.GraphicsDevice);
         // TODO: Add your initialization logic here
-
+        ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
         base.Initialize();
     }
 
@@ -27,15 +33,22 @@ public class DesktopFriend : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+        using var stream = Content.OpenStream("ball.png");
+        ballTexture = Texture2D.FromStream(GraphicsDevice, stream);
+
         // TODO: use this.Content to load your game content here
     }
 
     protected override void Update(GameTime gameTime)
     {
+
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
         {
             Exit();
         }
+
+        var state = Mouse.GetState();
+        ballPosition = state.Position.ToVector2();
 
         // TODO: Add your update logic here
         base.Update(gameTime);
@@ -46,6 +59,9 @@ public class DesktopFriend : Game
         GraphicsDevice.Clear(Color.Coral);
 
         // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(ballTexture, ballPosition, new Rectangle(0, 0, ballTexture.Width, ballTexture.Height), Color.White);
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
